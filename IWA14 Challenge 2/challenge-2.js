abc87f0bd5aa99797f64bc0df1517d1c68a6bdae
Rhys Pillay <Rhys.pillay@gmail.com>
2023-10-24 15:20:35 +0200

    challenge 2 solution

---
+++ IWA14 Challenge 2/challenge-2.js
@@ -0,0 +1,35 @@
+const add = function (a, b, c) {
+  return (a + b) * c;
+};
+
+const internal = function () {
+  total = this.add(this.internal.a, this.internal.b, this.internal.c);
+  console.log(total);
+};
+
+// Not allowed to change below this
+
+const example1 = {
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+const example2 = {
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+example1.calculate();
+example2.calculate();
